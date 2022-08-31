c267f45b1f75544c3908ea96fbbcaaf6957d90de
charan200415 <66833883+charan200415@users.noreply.github.com>
2022-08-31 12:10:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,18 @@ app = flask.Flask(__name__)
 def profile():
     print("working")
     return flask.request.url
+    url=(flask.request.url).replace('.shortu.ml/','').replace('https://','')
+    return url
+    ret=requests.get(f'https://shortu.ml/{url}?Pa=djgkfdghbfgbghkfndgkjnfghknkjghnkjdghkjn').text
+    if ret=='Not Found':
+        return flask.redirect(f'https://shortu.ml/{url}')
+    else:
+        return flask.redirect(ret)
     
 
 @app.route("/test")  # now the subdomain will be passed into the parameter 'username'
 def test():
     return "Hello Test Passed !"
+@app.route("/<xx>")  # now the subdomain will be passed into the parameter 'username'
+def xx(xx):
+    return xx
